39ddc6c6ea906420d96e1a600e8f8f4374c5fc04
Jay <dhananjay.shah244@gmail.com>
2022-11-27 22:49:05 +0100

    Update fixture to return Solution object instead of only a grid.
    + add docstrings to tests.

--- 934_shortest_bridge/test_solutions.py
+++ 934_shortest_bridge/test_solutions.py
@@ -1,21 +1,22 @@
 import pytest
-
-from .solutions import Solution
+from solutions import Solution
 
 
 @pytest.fixture()
-def grid() -> list[list[int]]:
-    return [
+def sample_grid() -> Solution:
+    """Return object under test initialized using a grid."""
+    grid = [
         [1, 1, 1, 0, 0],  # 0
         [1, 1, 0, 0, 0],  # 1
         [0, 0, 0, 0, 0],  # 2
         [0, 0, 1, 0, 0],  # 3
         [0, 1, 1, 1, 0],  # 4
-        #        0 1 2 3 4
+#        0  1  2  3  4
     ]
+    return Solution(grid)
 
 
-def test_neighbours(grid: list[list[int]]):
-    solution = Solution(grid)
-    neighbours = solution._neighbours(row=4, col=2)
+def test_neighbours(sample_grid: Solution):
+    """Validate that neighbours to a point are correctly identified."""
+    neighbours = sample_grid._neighbours(row=4, col=2)
     assert neighbours == [[4, 1], [3, 2], [4, 3]]
